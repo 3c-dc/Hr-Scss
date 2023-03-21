53af701d5f0438c3ba8ea96e2ea4bf676ac88030
dc <1304561865@qq.com>
2023-03-21 23:00:28 +0800

    完成封装单独的登录接口

--- src/api/user.js
+++ src/api/user.js
@@ -1,7 +1,13 @@
-// import request from '@/utils/request'
+import request from '@/utils/request'
 
 export function login(data) {
-
+  // 返回一个 axios 对象 => promise
+  // 返回了一个 promise 对象
+  return request({
+    url: '/sys/login', // 因为所有的接口都要跨域,表示所有的接口要带/api
+    method: 'post',
+    data: data // 因为两个对象名字一样可以直接使用ES6语法直接解构赋值 data:data === data
+  })
 }
 
 export function getInfo(token) {
