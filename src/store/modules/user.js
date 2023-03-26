ec3f43b6c6eeff3ad1123b6490ae24b4a38e6b9e
dc <1304561865@qq.com>
2023-03-26 23:28:27 +0800

    完成封装获取用户资料的action并共享用户状态

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,10 +1,11 @@
 import { getToken, setToken, removeToken } from '@/utils/auth'
-import { login } from '@/api/user'
+import { login, getUserInfo } from '@/api/user'
 // 状态
 // 初始化的时候从缓存中读取状态 并赋值到初始化的状态上
 // Vuex的持久化 如何实现? => Vuex和前端缓存相结合
 const state = {
-  token: getToken() // 色湖之token初始状态 token持久化 => 放到缓存中
+  token: getToken(), // 色湖之token初始状态 token持久化 => 放到缓存中
+  userInfo: {} // 定义一个空的对象 不是null 因为后边我要开发userInfo的属性给别人用  userInfo.name
 }
 // 修改状态
 const mutations = {
@@ -14,15 +15,28 @@ const mutations = {
     // Vuex变化 => 缓存数据
     setToken(token) // Vuex和缓存数据的同步
   },
+
   // 删除token 并删除缓存中的token
   removeToken(state) {
     state.token = null // 删除Vuex的token
     removeToken() // 先清除 Vuex 再清除缓存 Vuex和缓存数据的同步
+  },
+
+  // 设置用户信息
+  setUserInfo(state, userInfo) {
+    state.userInfo = userInfo // 这个方法也能触发组件的更新 实现响应式
+    // state.userInfo = { ...userInfo } // 用浅拷贝的方式去赋值对象 因为这样数据更新之后，才会触发组件的更新
+    // state.userInfo['username'] = result // 这样才不是响应式 触发组件更新
+  },
+
+  // 删除用户信息
+  reomveUserInfo(state) {
+    state.userInfo = {}
   }
 }
 // 执行异步
 const actions = {
-  // 定义login action 也需要参数 调用action时 传递过来的参数
+  // 定义登录login action 也需要参数 调用action时 传递过来的参数
   async login(context, data) {
     const result = await login(data) // 实际上就是一个promise  result就是执行的结果
     // axios默认给数据加了一层data
@@ -30,6 +44,13 @@ const actions = {
     // 现在有用户token
     // actions 修改state 必须通过mutations
     context.commit('setToken', result)
+  },
+
+  // 获取用户资料getUserInfo action
+  async getUserInfo(context) {
+    const result = await getUserInfo() // 获取返回值
+    context.commit('setUserInfo', result) // 将整个的个人信息设置到用户的Vuex数据
+    return result // 这里返回 为以后实现其他功能提供方便
   }
 }
 export default {
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,13 +1,31 @@
+// 导入 element-ui 的 message 组件
 import { Message } from 'element-ui'
+
 // 导出一个axios的实例  而且这个实例要有请求拦截器 响应拦截器
 import axios from 'axios'
+
+// 导入 Vuex store
+import store from '@/store'
+
 const service = axios.create({
   // 如果执行npm run dev的值为/api 正确 /api这个代理知识给开发环境配置的代理
   // 如果执行npm run build的值为/prod-api 没关系 运维应该在上线的时候 给你配置上/prod-api的代理
   baseURL: process.env.VUE_APP_BASE_API, // 设置axios请求的基础的基础地址
   timeout: 5000 // 定义5秒超时
 }) // 创建一个axios的实例
-service.interceptors.request.use() // 请求拦截器
+
+service.interceptors.request.use(config => {
+  // 在这个位置需要统一的去注入token
+  if (store.getters.token) {
+    // 如果token存在,注入token
+    config.headers['Authorization'] = `Bearer ${store.getters.token}`
+    return config // 必须返回配置 不然相当于没有配置
+  }
+}, error => {
+  return Promise.reject(error)
+}
+) // 请求拦截器
+
 service.interceptors.response.use(response => {
   // axios默认添加了一层data
   const { success, message, data } = response.data
