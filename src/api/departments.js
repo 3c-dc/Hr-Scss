1c2741a33f5546e75961600aa5324f84f3fc11e4
dc <1304561865@qq.com>
2023-04-12 22:31:54 +0800

    完成编辑部门功能实现,并且解决(Date: Tue Apr 11 09:26:47 2023 +0800 完成新增部门的规则校验(自定义校验部门名称有BUG后续解决).)的BUG

--- src/api/departments.js
+++ src/api/departments.js
@@ -22,13 +22,33 @@ export function delDepartments(id) {
 }
 
 /**
- *  新增部门接口
  *
+ * 新增部门接口
  * */
 export function addDepartments(data) {
   return request({
     url: '/company/department',
     method: 'post',
+    data
+  })
+}
+/**
+ *
+ * 获取部门详情
+ */
+export function getDepartDetail(id) {
+  return request({
+    url: `/company/department/${id}`
+  })
+}
+/**
+ *
+ * 编辑部门
+ */
+export function updateDepartments(data) {
+  return request({
+    url: `/company/department/${data.id}`,
+    method: 'put',
     data
   })
 }
