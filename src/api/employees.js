ecbb442c503bab35618661b7d472de6732ddf093
dc <1304561865@qq.com>
2023-04-28 13:49:23 +0800

    完成员工管理员工详情之查看,其中的员工的姓名和密码修改

--- src/api/employees.js
+++ src/api/employees.js
@@ -54,3 +54,15 @@ export function importEmployee(data) {
     data
   })
 }
+
+/**
+ *
+ * 保持员工的基本信息
+ */
+export function saveUserDetailById(data) {
+  return request({
+    url: `/sys/user/${data.id}`,
+    method: 'put',
+    data
+  })
+}
